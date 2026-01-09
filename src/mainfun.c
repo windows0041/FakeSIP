c10a9be95d0e0dfbf002df5f0e466b5bff3fad8b
coutps <119647405+windows0041@users.noreply.github.com>
2026-01-10 01:18:01 +0800

    error

--- src/mainfun.c
+++ src/mainfun.c
@@ -282,31 +282,31 @@ int main(int argc, char *argv[])
             
             case 'p':
             case 'P': {
-                if (!optarg[0]) {
-                    fprintf(stderr,
-                            "%s: value of -%c cannot be empty.\n",
-                            argv[0], opt);
-                    print_usage(argv[0]);
-                    goto free_mem;
-                }
-            
-                if (opt == 'p') {
-                    if (fs_portmap_parse(&g_ctx.port_white, optarg) < 0) {
-                        fprintf(stderr,
-                                "%s: invalid port spec for -p: %s\n",
-                                argv[0], optarg);
-                        goto free_mem;
-                    }
-                } else {
-                    if (fs_portmap_parse(&g_ctx.port_black, optarg) < 0) {
-                        fprintf(stderr,
-                                "%s: invalid port spec for -P: %s\n",
-                                argv[0], optarg);
-                        goto free_mem;
-                    }
-                }
-            }
-            break;
+    if (!optarg[0]) {
+        fprintf(stderr,
+                "%s: value of -%c cannot be empty.\n",
+                argv[0], opt);
+        print_usage(argv[0]);
+        goto free_mem;
+    }
+
+    if (opt == 'p') {
+        if (fs_portmap_parse(&g_ctx.port_white, optarg) < 0) {
+            fprintf(stderr,
+                    "%s: invalid port spec for -p: %s\n",
+                    argv[0], optarg);
+            goto free_mem;
+        }
+    } else {
+        if (fs_portmap_parse(&g_ctx.port_black, optarg) < 0) {
+            fprintf(stderr,
+                    "%s: invalid port spec for -P: %s\n",
+                    argv[0], optarg);
+            goto free_mem;
+        }
+    }
+}
+break;
 
             case 'x':
                 tmp = strtoull(optarg, NULL, 0);
